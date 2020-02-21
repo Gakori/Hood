3328e4950df17393b10a58d19cf1ee53c6774888
faith gakori <faithgakori506@gmail.com>
2020-02-21 12:07:09 +0300

    add register routes

--- hoodie/views.py
+++ hoodie/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.http import HttpResponse
+
+from django.contrib.auth.forms import UserCreationForm
+
+from .forms import UserRegisterForm
+
+from django.contrib import messages
 
 def home(request):
     return HttpResponse('kjhgfcdgh')
     
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'auth/register.html', {'form': form})
