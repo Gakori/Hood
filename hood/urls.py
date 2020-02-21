e12c625b8aac09702316bb69c72c69b39ee817f6
faith gakori <faithgakori506@gmail.com>
2020-02-21 12:08:53 +0300

    add login,logout,register url

--- hood/urls.py
+++ hood/urls.py
@@ -30,4 +30,10 @@ urlpatterns = [
     
     path('', views.home, name='home'),
     
+    path('register/', hoodie_views.register, name='register'),
+
+    path('login/', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
+
+    path('logout/', auth_views.LogoutView.as_view(template_name='auth/logout.html'), name='logout'),
+    
 ]
