54db71739685c70636cb242ce70a29c5cc54ed4d
faith gakori <faithgakori506@gmail.com>
2020-02-21 20:34:21 +0300

    migrate

--- hoodie/forms.py
+++ hoodie/forms.py
@@ -1,6 +1,10 @@
 from django import forms
 
+from .models import Post
+
 from django.contrib.auth.forms import UserCreationForm
+
+from pyuploadcare.dj.forms import ImageField
 
 from django.contrib.auth.models import User
 
@@ -10,3 +14,10 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        
+class PostForm(forms.ModelForm):
+    photo = ImageField(label='')
+
+    class Meta:
+        model = Post
+        fields = ('photo',)
--- hoodie/urls.py
+++ hoodie/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+from .views import PostCreateView
+
 urlpatterns = [
-    path('', views.home, name='home') ,
+    path('', views.home, name='home'),
+    
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
 ]
