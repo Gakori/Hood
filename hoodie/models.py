099ea53dda5129e034c19dccdac587cf27f4bae2
faith gakori <faithgakori506@gmail.com>
2020-02-21 20:31:56 +0300

    add pic field

--- hoodie/models.py
+++ hoodie/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+from django.utils import timezone
+
 from django.contrib.auth.models import User
+
+from pyuploadcare.dj.models import ImageField
+
+class Post(models.Model):
+    photo = ImageField(blank=True, manual_crop="")
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    location = models.CharField(max_length=100)
+    date_posted = models.DateField(default=timezone.now)
+    
+    def __str__(self):
+        return self.name
+    
