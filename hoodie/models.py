ad19e3ff596ec79dbcc97fc50dadce937f862b3c
faith gakori <faithgakori506@gmail.com>
2020-02-23 16:19:38 +0300

    add class profile

--- hoodie/models.py
+++ hoodie/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 from django.utils import timezone
 
+from django.urls import reverse
+
 from django.contrib.auth.models import User
 
 from pyuploadcare.dj.models import ImageField
@@ -11,8 +13,19 @@ class Post(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=200)
     location = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     date_posted = models.DateField(default=timezone.now)
     
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk':self.pk})
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
     
